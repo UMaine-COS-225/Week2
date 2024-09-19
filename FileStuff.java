b0420a40a80c519ae0b07eac027a35b247f1ff5d
Vijayanta Jain <vijayantajain@gmail.com>
2024-09-19 01:43:39 -0400

    Resolving bug and adding more code examples

--- FileStuff.java
+++ FileStuff.java
@@ -1,9 +1,15 @@
-import java.io.*;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 class FileStuff {
 
     public static void main(String[] args) {
-        
+
         // Writing to a file
         try {
             FileWriter writer = new FileWriter("hello.txt");
@@ -14,6 +20,7 @@ class FileStuff {
             e.printStackTrace();
         }
 
+        // Writing with try-with-resources which automatically close the files for us!
         try (FileWriter writer = new FileWriter("hello.txt")) {
             writer.write("Hello World\n");
         } catch (IOException e) {
@@ -21,14 +28,37 @@ class FileStuff {
             e.printStackTrace();
         }
 
-        //Reading file
-        try (BufferedReader reader = new BufferedReader(new FileReader("hello.txt"))){
+        // Reading file
+        try (BufferedReader reader = new BufferedReader(new FileReader("hello.txt"))) {
             String line = reader.readLine();
-            while(line != null) {
+            while (line != null) {
                 System.out.println(reader.readLine());
+                line = reader.readLine();
             }
         } catch (Exception e) {
             System.out.println("Something happened when trying to read the file");
+            e.printStackTrace();
+        }
+
+        // Reading file without BufferedReader
+        try (FileReader reader = new FileReader("hello.txt")) {
+            int character;
+            while ((character = reader.read()) != -1) {
+                System.out.print((char) character);
+            }
+        } catch (IOException e) {
+            System.out.println("Something happened when trying to read the file");
+            e.printStackTrace();
+        }
+
+        // Reading file using Scanner
+        try (Scanner scanner = new Scanner(new File("hello.txt"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
             e.printStackTrace();
         }
 
